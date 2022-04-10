2e1ed445adfaba872a9f82981b33643911bce936
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-04-10 03:58:10 +0000

    First commit

---
+++ cms.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+  set :erb, :escape_html => true
+end
+
+helpers do
+end
+
+before do
+end
+
+get "/" do
+  
+end
